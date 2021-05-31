f19f7fbe1bb17c2a1ddba5df4d4a5daae623778d
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-06-01 01:15:58 +0300

    Create GoldDataset.py

---
+++ GoldDataset.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pandas as pd
+
+X=pd.read_csv('../dataset/Gold Futures Historical Data.csv')
+
+class GoldDataset():
+    
+    def __init__(self):
+        
+        self.folderName='../dataset/Gold Futures Historical Data.csv'
+        self.data=pd.read_csv(folderName)
+        self.columns=self.data.columns
+        
+    def prepareTimeSeriesData(self,model='regression',time=30):
+        
+        
+        X=np.zeros((self.data.shape[0]-time,time))
+        y=np.zeros((X.shape[0],1))
+        
+        for i in range(y.shape[0]):
+            X[i,:]=self.data['Price'][i:i+time]
+            if method=='regression':
+                y[i]=self.data['Price'][i+time]
