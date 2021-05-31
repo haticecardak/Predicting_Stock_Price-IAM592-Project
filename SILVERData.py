032f96144a62f5e491ae01044c02fa8b923b9060
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-06-01 01:00:05 +0300

    Update SILVERData.py

--- SILVERData.py
+++ SILVERData.py
@@ -10,7 +10,7 @@ class SilverData():
         self.columns=self.data.columns
   
     def prepareTimeSeriesData(self,model='regression',time=30):
-        # a=
+       
         X=np.zeros((self.data.shape[0]-time,time))
         y=np.zeros((X.shape[0],1))
         
