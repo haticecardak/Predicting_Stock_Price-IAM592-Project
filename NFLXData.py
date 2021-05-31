222511e165b308c37ad446c07afe35541c0b7385
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-06-01 01:01:05 +0300

    Update NFLXData.py

--- NFLXData.py
+++ NFLXData.py
@@ -11,7 +11,7 @@ class NetFlixData():
         self.data=pd.read_csv(self.folderName)
         self.columns=self.data.columns
   
-    def prepareTimeSeriesData(self,method='regression',time): 
+    def prepareTimeSeriesData(self,model='regression',time): 
      
         X=np.zeros((self.data.shape[0]-time,time))
         y=np.zeros((X.shape[0],1))
@@ -20,9 +20,6 @@ class NetFlixData():
             X[i,:]=self.data['Open'][i:i+time]
             if method=='regression':
                 y[i]=self.data['Open'][i+time]
-            if method=='classification':
-                y[i]=self.data['High'][i+time]/self.data['Open'][i+time-1]
-              
                 
         return np.flip(X,axis=0),np.flip(y,axis=0)
         
@@ -30,5 +27,5 @@ class NetFlixData():
         return np.flip(self.data['Open'].values)
     
 nflx=NetFlixData()
-X,y=nflx.prepareTimeSeriesData(method='regression',time = 30)
+X,y=nflx.prepareTimeSeriesData(model='regression',time = 30)
 
