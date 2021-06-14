b7b221cbfb90af24aec4ea71ab5d6fcf02608619
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-06-14 06:27:47 +0300

    Update LinearReg.py

--- LinearReg.py
+++ LinearReg.py
@@ -4,13 +4,12 @@ import statsmodels.api as sm
 
 class LinearReg():
     
-    def __init__(self,standardize,method,lam):
+    def __init__(self,standardize,method,lamda):
         self.standardize=standardize
         self.method=method
-        self.lam=lam
+        self.lamda=lamda
         def fit(self,X,y):
         '''
-        
         Parameters
         ----------
         X : feature matrix
@@ -20,12 +19,26 @@ If we need to standardize the data, it would hold the data mean and standard dev
         -----------
 
         '''
-        if self.standardize:
+ if self.standardize:
             self.mx=X.mean(axis=0) # calculate the average of X matris axis=0 means that only rows average taking into considiretion
-            self.sx=X.std(axis=0) #calculate the standar deviation of the X matrix
+            self.sx=X.std(axis=0) #calculate the standard deviation of the X matrix
             
             X=(X-self.mx)/self.sx #normalization
             
             self.my=y.mean() #calculate the target mean
             y=y-self.my #make centeralization
             
+        if self.method=='OrdinaryLeastSquare': #ordinary least square
+            self.B=np.dot(np.linalg.inv(np.dot(X.T,X)),np.dot(X.T,y)) # beta coefficient of i nth day 
+        
+        if self.method=='Ridge':
+            self.B=np.dot(np.linalg.inv(np.dot(X.T,X)+self.lam*np.eye(X.shape[1])),np.dot(X.T,y))
+        
+    def predict(self,X):
+        if self.standardize:
+            X=(X-self.mx)/self.sx #normalization 
+        
+            return (self.my+np.dot(X,self.B))
+        
+        else:
+            return np.dot(X,self.B)
