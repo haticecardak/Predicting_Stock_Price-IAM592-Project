f926601397478fb7df42eceffae2885739fa06b2
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-06-14 06:57:12 +0300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -32,8 +32,39 @@ for key in data.keys():
 
 
 #--Linear Regression 
- 
-   for key in data.keys():
+  
+  lrOLS=LinearRegression(standardize=True,method='OLS',lam=0)
+  lrRidge=LinearRegression(standardize=True,method='Ridge',lam=10)
+  
+    for key in data.keys():
         
         X,y = data[key].prepareTimeSeriesData(model='regression',time=30)
         xtrain,ytrain,xtest,ytest = splitDataset(X,y,ratio=0.9)
+          xtrain,ytrain,xtest,ytest = splitDataset(X,y,ratio=0.9)
+        sTime=time.time() #measure the time from the time library
+        lrOLS.fit(xtrain,ytrain)
+        print('OLS Training ({} Samples) Time  : {:.3f} seconds'.format(xtrain.shape[0],time.time()-sTime))
+        ypred=lrOLS.predict(xtest)
+        
+        print('{} OLS test MSE : {}'.format(key,np.mean((ypred-ytest)**2)))
+        plt.figure()
+        plt.plot(ytest)
+        plt.plot(ypred)
+        plt.title('{} Test Data OLS Estimation'.format(key))
+        plt.savefig('../images/{} OLS Estimation '.format(key))
+         
+        plt.show()
+        
+        sTime=time.time() 
+        lrRidge.fit(xtrain,ytrain)
+        print('Ridge Training ({} Samples) Time  : {:.3f} seconds'.format(xtrain.shape[0],time.time()-sTime))
+        ypred=lrRidge.predict(xtest)
+        
+        print('{} Ridge test MSE : {:.3f}'.format(key,np.mean((ypred-ytest)**2)))
+        plt.figure()
+        plt.plot(ytest)
+        plt.plot(ypred)
+        plt.title('{} Test Data Ridge Estimation'.format(key))
+        plt.savefig('../images/{} Ridge Estimation '.format(key))
+        plt.show()
+
