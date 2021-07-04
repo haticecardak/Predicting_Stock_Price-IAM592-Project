81b2449f9e2f0f96147e5398a531fb1403d2975c
haticecardak <78654515+haticecardak@users.noreply.github.com>
2021-07-04 15:57:54 +0300

    Update NFLXData.py

--- NFLXData.py
+++ NFLXData.py
@@ -2,20 +2,19 @@
 import numpy as np 
 import pandas as pd
 
-X=pd.read_csv('../dataset/NFLX_Histdata.csv')
-X=pd.read_csv('../data/NFLX_Histdata.csv' )
+X=pd.read_csv('../data/NFLX_Histdata.csv')
 X.columns = ["Date","Open","High","Low","Close","Adj Close","Volume"]
 X.head()
 print(X)
 
-class NetFlixData():
+class NetFlixDataset():
     def __init__(self):
-        self.folderName='../dataset/NFLX_Histdata.csv'
+        self.folderName='../data/NFLX_Histdata.csv'
         self.data=pd.read_csv(self.folderName)
         self.columns=self.data.columns
   
-    def prepareTimeSeriesData(self,model='regression',time): 
-     
+    def prepareTimeSeriesData(self,method='regression',time=30): 
+        # a=
         X=np.zeros((self.data.shape[0]-time,time))
         y=np.zeros((X.shape[0],1))
         
@@ -23,12 +22,26 @@ class NetFlixData():
             X[i,:]=self.data['Open'][i:i+time]
             if method=='regression':
                 y[i]=self.data['Open'][i+time]
+            if method=='classification': 
+                y[i]=self.data['High'][i+time]/self.data['Open'][i+time-1]
+                
                 
         return np.flip(X,axis=0),np.flip(y,axis=0)
         
     def getCloseData(self):
-        return np.flip(self.data['Open'].values)
+          return np.flip(self.data['Open'].values) 
+print('\n Open Prices: \n' ,np.flip(X['Open'].values))
     
-nflx=NetFlixData()
-X,y=nflx.prepareTimeSeriesData(model='regression',time = 30)
+netflix=NetFlixDataset()
+X,y=netflix.prepareTimeSeriesData(method='regression',time = 30)
+
+print(' \n Regression Time Data set: \n',X,
+      '\n Open Price Data set: \n',y)
+
+
+X2,y2=netflix.prepareTimeSeriesData(method='classification',time = 30)
+print('\n Classification Time Data Set: \n', X2,
+      '\n Predicted Prices: \n', y2)
+
+print('\n The average of the predicted price data set: ',(y2>1.01).mean())
 
